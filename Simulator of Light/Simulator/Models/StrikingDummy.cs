1cf42ab5f05a27f763f502a061a1f6ea65cc5840
Dylan Walker <koarth@hotmail.ca>
2018-06-25 08:48:16 -0400

    Interface compliance

--- Simulator of Light/Simulator/Models/StrikingDummy.cs
+++ Simulator of Light/Simulator/Models/StrikingDummy.cs
@@ -17,15 +17,25 @@ namespace Simulator_of_Light.Simulator.Models {
 
         }
 
-        public void ApplyDamage() {
-            throw new NotImplementedException();
-        }
-
         public string Name { get; private set; }
         public long CurrentHP { get; private set; }
         public long MaxHP { get; private set; }
 
         public SortedList<long, Aura> Auras { get; }
+
+        public void ApplyDamage() {
+            throw new NotImplementedException();
+        }
+
+        public Aura GetAuraByName(string auraName) {
+            foreach (Aura aura in this.Auras.Values) {
+                if (aura.BaseAura.Name == auraName) {
+                    return aura;
+                }
+            }
+
+            return null;
+        }
 
     }
 }
