86231c12bac8d441b19e57aca099b8f464214b69
Dylan Walker <koarth@hotmail.ca>
2018-07-10 12:23:09 -0400

    Some documentation, fix crit and direct hit rate calcs

--- Simulator of Light/Simulator/BattleCalculations.cs
+++ Simulator of Light/Simulator/BattleCalculations.cs
@@ -12,6 +12,15 @@ namespace Simulator_of_Light.Simulator {
 
         public static Random r = new Random();
 
+        /// <summary>
+        /// Contextually calculate the damage of an action before critical or direct hit
+        /// calculations and before 5% variance. Requires initialized and active source 
+        /// actor and target, as well as the action information.
+        /// </summary>
+        /// <param name="action">The action being used.</param>
+        /// <param name="source">The source actor using the action.</param>
+        /// <param name="target">The target being affected by the action.</param>
+        /// <returns>Damage before RNG effects.</returns>
         public static int CalculateDamage(Action action, IActor source, ITarget target) {
 
             var potency = action.BaseAction.Potency;
@@ -68,6 +77,17 @@ namespace Simulator_of_Light.Simulator {
             return (int)damage;
         }
 
+        /// <summary>
+        /// Contextually calculate the damage of an aura before critical or direct hit
+        /// calculations and before 5% variance. Requires initialized and active source 
+        /// actor and target, as well as the aura information.  This should be calculated
+        /// on application of the aura, and attached to the active aura, as aura effects
+        /// snapshot temporary effects (e.g. Trick Attack).
+        /// </summary>
+        /// <param name="aura">The aura being applied.</param>
+        /// <param name="source">The source actor using the action.</param>
+        /// <param name="target">The target being affected by the action.</param>
+        /// <returns>Damage before RNG effects.</returns>
         public static int CalculateDamage(BaseAura aura, IActor source, ITarget target) {
 
             var potency = aura.DamageOverTimePotency;
@@ -126,6 +146,21 @@ namespace Simulator_of_Light.Simulator {
             return (int)damage;
         }
 
+        /// <summary>
+        /// Contextually calculates the critical hit rate for an action.
+        /// This will account for aura effects on the source actor, and the 
+        /// target if applicable.  
+        /// 
+        /// This function will defer to the source actor to determine the
+        /// base critical hit rate before auras, as a subclass of actor
+        /// may adjust the outgoing critical hit rate contextually.
+        /// (e.g. Warrior's beast gauge effects crit rate, Bootshine is a 
+        /// guaranteed crit when performed from the rear in Opo-opo Form, etc.)
+        /// </summary>
+        /// <param name="source">Source actor performing the action.</param>
+        /// <param name="target">Target the action will affect.</param>
+        /// <param name="action">The action being used.</param>
+        /// <returns>The current critical hit rate for the action being used.</returns>
         public static double CalculateCriticalHitRate(IActor source, ITarget target, Action action) {
 
             double rate = source.getCriticalHitRate(action);
@@ -134,23 +169,53 @@ namespace Simulator_of_Light.Simulator {
                 rate += aura.BaseAura.CriticalHitRateModifier;
             }
 
-            return Math.Min(1, rate);
-        }
-
-        public static double CalculateDirectHitRate(IActor source, ITarget target, Action action) {
-
-            double rate = source.getDirectHitRate();
-
-            foreach (Aura aura in source.Auras) {
-                rate += aura.BaseAura.IncomingCriticalHitRateModifier;
+            if (target != null) {
+                foreach (Aura aura in target.Auras) {
+                    rate += aura.BaseAura.IncomingCriticalHitRateModifier;
+                }
             }
 
             return Math.Min(1, rate);
         }
 
-        public static bool RollConditionalSuccess(double rate) {
 
-            // Proc rates use 1 decimal of precision (XX.X% success rate)
+        /// <summary>
+        /// Contextually calculates the direct hit rate for an action.
+        /// This will account for aura effects on the source actor, and the 
+        /// target if applicable.  
+        /// 
+        /// This function will defer to the source actor to determine the
+        /// base critical hit rate before auras, as a subclass of actor
+        /// may adjust the outgoing direct hit rate contextually.
+        /// There are no non-aura effects that do this currently, but
+        /// they may be added in the future.
+        /// <param name="source">Source actor performing the action.</param>
+        /// <param name="target">Target the action will affect.</param>
+        /// <param name="action">The action being used.</param>
+        /// <returns>The current direct hit rate for the action being used.</returns>
+        public static double CalculateDirectHitRate(IActor source, ITarget target, Action action) {
+
+            double rate = source.getDirectHitRate();
+
+            foreach (Aura aura in source.Auras) {
+                rate += aura.BaseAura.DirectHitRateModifier;
+            }
+
+            return Math.Min(1, rate);
+        }
+
+        /// <summary>
+        /// Basic function to roll for a success or failure against the
+        /// given proc rate.  The rate given can be a critical hit rate,
+        /// a direct hit rate, or an individual action's (e.g. Verfire).
+        /// 
+        /// Rates should be given as a double between 0 and 1, and are
+        /// expected to have a precision of 3 decimal places.
+        /// (XX.X% success rate)
+        /// </summary>
+        /// <param name="rate">The success rate to be rolled against.</param>
+        /// <returns>True if the roll succeeded, false otherwise.</returns>
+        public static bool RollConditionalSuccess(double rate) {
             int maximum = (int)(rate * 1000);
             int roll = BattleCalculations.r.Next(1000) + 1;
 
