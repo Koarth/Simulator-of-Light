ffb501c78b20e021df12014416249976ac1376bf
Dylan Walker <koarth@hotmail.ca>
2018-07-11 16:16:09 -0400

    Healing events.
    * Event classes should roughly correspond to FFLogs tabs, probably

--- Simulator of Light/Simulator/Models/CombatLogEvents/HealingEvent.cs
+++ Simulator of Light/Simulator/Models/CombatLogEvents/HealingEvent.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Simulator_of_Light.Simulator.Models.CombatLogEvents {
-    public class DamageEvent : CombatLogEvent {
+    public class HealingEvent : CombatLogEvent {
 
         public ITarget Target { get; set; }
 
@@ -13,27 +13,25 @@ namespace Simulator_of_Light.Simulator.Models.CombatLogEvents {
         public BaseAction Action { get; set; }
         public BaseAura Aura { get; set; }
 
-        // Damage values.
-        public int RawDamage { get; set; }
-        public int EffectiveDamage { get; set; }
-        public int AbsorbedDamage { get; set; }
-        public int BlockedDamage { get; set; }
-        public int OverkillDamage { get; set; }
+        // Healing values.
+        public int RawHealing { get; set; }
+        public int EffectiveHealing { get; set; }
+        public int AbsorbedHealing { get; set; }
+
+        public HealingEvent(long time, IActor source, ITarget target, BaseAction action) : base(time, source) {
+            this.Target = target;
+            this.Action = action;
+        }
 
         // Modifiers.
         public bool IsCritical { get; set; }
         public bool IsDirectHit { get; set; }
         public bool IsTick { get; set; }
 
-        public DamageEvent(long time, IActor source, ITarget target, BaseAction action) : base(time, source) {
-            this.Target = target;
-            this.Action = action;
-        }
-
         public override string ToString() {
-
-            // TODO
             throw new NotImplementedException();
         }
+
+
     }
 }
